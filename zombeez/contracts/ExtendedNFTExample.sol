76bc869157d24be87e87cee9fdee5078d5d0abf6
emdavis7 <ericd@simpli.fi>
2021-11-05 00:59:42 -0500

    safemath is obsolete

--- zombeez/contracts/ExtendedNFTExample.sol
+++ zombeez/contracts/ExtendedNFTExample.sol
@@ -5,13 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import '@openzeppelin/contracts/utils/Counters.sol';
 
 
 contract ExtendedNFTTemplate is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
-    using SafeMath for uint;
-    using Strings for uint;
+    using Strings for uint256;
     using Address for address;
     using Counters for Counters.Counter;
 
